63c839c4b52da25c67c822f8ea52effb30537237
katiechambe <katiechambe@email.arizona.edu>
2021-08-25 15:03:46 -0700

    .

--- pears/utils/readsubfHDF5Py3.py
+++ pears/utils/readsubfHDF5Py3.py
@@ -26,8 +26,8 @@ __status__ = "Beta -- forever."
 import os
 import sys
 import numpy as np
-import hdf5libPy3 as hdf5lib
-import namingPy3 as naming
+import utils.hdf5libPy3 as hdf5lib
+import utils.namingPy3 as naming
 
 
 ####################
