6d7e404aa61cf323b3216451123193eddeef24b5
katiechambe <katiechambe@email.arizona.edu>
2021-09-14 11:33:02 -0700

    she works on both dark and hydro im crying

--- pears/utils/get_groups.py
+++ pears/utils/get_groups.py
@@ -7,17 +7,14 @@ ReadCats class from read_group_cats.py
 SetupPaths class from paths.py
 
 """
-
 __author__ = "Katie Chamberlain"
-__status__ = "Beta -- forever."
+__status__ = "Beta - forever~"
 
 import numpy as np
 from utils.read_group_cats import ReadCats
 from utils.paths import SetupPaths
 from astropy.table import QTable
 import astropy.units as u
-# import pandas as pd
-
 
 class GetGroups:
 
@@ -108,33 +105,9 @@ class GetGroups:
         t = QTable()
         t['group_number'] = group_number
         t['group_mass'] = group_mass
-        t['group_radius'] = 
-        t['group_nsubs'] = 
-        self.pass_numbers.info.description
-        t.write(self.path_groups + save_path)
-
-
-        # zipped = list(zip(self.pass_numbers, 
-        #                   self.pass_mvir*u.Unit(1e10 * u.Msun), 
-        #                   self.pass_rvir*u.kpc, 
-        #                   self.pass_nsubs))
-
-        # df = pd.DataFrame(data=zipped, 
-        #     columns=["Group Number","Group Mass","",""])
-
-        df = pd.DataFrame(data = )
+        t['group_radius'] = group_radius
+        t['group_nsubs'] = group_nsubs
+        t.write(self.path_groups + save_path,
+                overwrite=True)
 
         print(f"Saved groups at {self.sim}_{self.physics}_{self.size}_{self.snapshot}.csv")
-
-
-print('got groups')
-
-#######################################
-# collecting the groups and exporting #
-#######################################
-# groupNumbers = np.array(groups)
-df = pd.DataFrame(data = zipped, columns=['Group Numbers','Group Mass','Group Radius','Isolated Flag Hill','Perturbing Group Number','Perturbing Group Mass Ratio','Perturbing Group Separation'])
-# zipped = list(zip(groups, groupMasses, isoFlag0p5Mpc, tidalIndex0p5Mpc, isoFlag1Mpc, tidalIndex1Mpc, isoFlag1p5Mpc, tidalIndex1p5Mpc, pertGroupNum, pertMassRatio, pertSep))
-# df = pd.DataFrame(data = zipped, columns=['Group Numbers','Group Mass','Isolated 0.5Mpc Flag','Tidal Index 0.5Mpc','Isolated 1Mpc Flag','Tidal Index 1Mpc', 'Isolated 1.5Mpc Flag','Tidal Index 1.5Mpc','Perturbing Group Number','Perturbing Group Mass Ratio','Perturbing Group Separation'])
-# note, group mass is in 10^10 Msun
-df.to_csv(saveFile,index=False,header=True)
