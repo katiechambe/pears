f3b9f092323082f85be9ad2772b6fcc5192c3f20
katiechambe <katiechambe@email.arizona.edu>
2021-09-20 12:57:51 -0700

    added new heading info to script

--- pears/utils/vectorCorrection.py
+++ pears/utils/vectorCorrection.py
@@ -1,13 +1,19 @@
-import numpy as np
-
-'''
+""" 
 vectorCorrection corrects the separation vector between two subhalos
 accounting for the periodic boundary conditions of the simulation box. 
-Returns the corrected separation vector between the subhalos
+Returns:
+--------
+    the corrected separation vector between the subhalos
+
 Note: 
 ----- 
-Input all values in physical units (kpc)
-'''
+    Input all values in physical units (kpc)
+"""
+
+__author__ = "Katie Chamberlain"
+__date__   = "September 2021"
+
+import numpy as np
 
 def vectorCorrection(primaryPosition, secondaryPosition, boxsize):
     '''
