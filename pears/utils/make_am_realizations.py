92dc88a350bf1db8df4a0c456fe944073f5b9e34
katiechambe <katiechambe@email.arizona.edu>
2021-10-05 23:03:32 -0700

    added some lines - going to test on int node tmw if i can do arraylike max mass initialization

--- pears/utils/make_am_realizations.py
+++ pears/utils/make_am_realizations.py
@@ -20,78 +20,59 @@ from utils.paths import SetupPaths
 
 
 #TODO: define physics, simulation, and snapshot
-
+#TODO: allow for median in abundance matching code! as alt calc.
+sim = "Illustris" 
+physics = "dark"
+snapshot = 135
 
 paths = SetupPaths()
 
-read_path = f"{paths.path_maxmass}/{sim}_{phys}_{snapshot}.csv"
-save_path = f"{paths.path_am_mass}/{sim}_{phys}_{snapshot}.csv"
+# calculate the redshift at current snapshot
+snapdata_path = f"{paths.path_snapdata}{sim}_snapdata.csv"
+snapdata = QTable.read(snapdata_path)
+redshift = snapdata['redshift'][snapdata['Snapshot'] == snapshot]
+
+read_path = f"{paths.path_maxmass}{sim}_{phys}_{snapshot}.csv"
+
+subhalo_data = QTable.read(read_path)
 
 
 
+masses = np.array([1e10,1e12])
+
+stels = AbundanceMatching(masses, redshift, 10).stellarMass()
 
 
 
+# primID, primHaloMass, primStellars = [], [], []
+
+haloGroup, haloID, haloMass, haloMaxMass, stellarMedian, stellarGauss = [], [], [], [], [], []
+
+for ind in range(len(haloData)):
+    haloGroup.append(haloData['Group Number'][ind])
+    haloID.append(haloData['Subhalo ID'][ind])
+    haloMass.append(haloData['Mass at snap'][ind])
+    haloMaxMass.append(haloData['Max Mass'][ind]) # in 1e10 Msun
+
+    maxMass         = haloData['Max Mass'][ind]*1e10 # halo mass in Msun (not 1e10!)
+    maxMassRedshift = haloData['Redshift at Max Mass'][ind]
+    median          = AM(maxMass, red, 'median').stellarMass()/1e10
+    stellars        = AMGauss(maxMass, red, numRealizations).stellarMass()/1e10 # in 1e10Msun
+    stellarMedian.append(median)
+    stellarGauss.append(stellars)
+
+    if ind%1000 == 0:
+        print('on count ', ind, 'of ',len(haloData))
 
 
 
+columnNames = ['Group Number','Subhalo ID', 'Mass at snap', 'Max Mass','Median Stellar'] + ['Stellar %i' % i for i in range(numRealizations)]
+haloTable = np.column_stack([haloGroup, haloID, haloMass, haloMaxMass, stellarMedian, stellarGauss])
+print('Created table')
+haloDF = pd.DataFrame(data = haloTable, columns=columnNames)
+print('Created dataframe')
+print('Exporting to file')
+haloDF.to_csv(fileToSave,index=False,header=True)
+print('All done!')
 
-snap = int(sys.argv[1]) # reading in the snapshot number
-numRealizations = int(sys.argv[2])
-
-#####################################
-# defining the hardcoded paths, etc #
-#####################################
-baseDir      = '/xdisk/gbesla/mig2020/rsgrps/gbeslastudents/katie/dwarfPairsDark/'
-
-spacingFile = baseDir + 'data/raw/data_spacing.csv'
-primToRead = baseDir + 'data/processed/allPairs/primaries_'+str(snap)+'.csv'
-secoToRead = baseDir + 'data/processed/allPairs/secondaries_'+str(snap)+'.csv'
-
-primToSave = baseDir + 'data/processed/amRealizations/primaries_'+str(snap)+'.csv'
-secoToSave = baseDir + 'data/processed/amRealizations/secondaries_'+str(snap)+'.csv'
-
-#####################################################
-# defining the snapshots and redshifts in Illustris #
-#####################################################
-snapData  = pd.read_csv(spacingFile)
-snapshots = snapData['Snapshot'].values
-scales    = snapData['Scale'].values
-redshifts = snapData['Redshift'].values
-
-primData = pd.read_csv(primToRead)
-secoData = pd.read_csv(secoToRead)
-
-primID, primHaloMass, primStellars = [], [], []
-secoID, secoHaloMass, secoStellars = [], [], []
-
-for ind in range(len(primData)):
-    ## primaries first! ##
-    primID.append(primData['Subhalo ID'][ind])
-    primHaloMass.append(primData['Max Mass'][ind]) # in 1e10 Msun
-
-    # to get the number of realizations I want, need array of masses to feed into AM class
-    primMaxMass         = primData['Max Mass'][ind]*1e10 # halo mass in Msun (not 1e10!)
-    primMaxMassRedshift = primData['Redshift at Max Mass'][ind]
-    primStellarMasses   = np.array([(AM(primMaxMass, primMaxMassRedshift, 'gauss').stellarMass())[0]/1e10 for i in range(numRealizations)])
-    primStellars.append(primStellarMasses)
-
-    ## secondaries second! ##
-    secoID.append(secoData['Subhalo ID'][ind])
-    secoHaloMass.append(secoData['Max Mass'][ind]) # in 1e10 Msun
-
-    # to get the number of realizations I want, need array of masses to feed into AM class
-    secoMaxMass         = secoData['Max Mass'][ind]*1e10 # halo mass in Msun (not 1e10!)
-    secoMaxMassRedshift = secoData['Redshift at Max Mass'][ind]
-    secoStellarMasses   = np.array([(AM(secoMaxMass, secoMaxMassRedshift, 'gauss').stellarMass())[0]/1e10 for i in range(numRealizations)])
-    secoStellars.append(secoStellarMasses)
-
-columnNames = ['Subhalo ID', 'Max Mass'] + ['Stellar %i' % i for i in range(numRealizations)]
-
-primTable = np.column_stack([primID, primHaloMass, primStellars])
-primDF = pd.DataFrame(data = primTable, columns=columnNames)
-primDF.to_csv(primToSave,index=False,header=True)
-
-secoTable = np.column_stack([secoID, secoHaloMass, secoStellars])
-secoDF = pd.DataFrame(data = secoTable, columns=columnNames)
-secoDF.to_csv(secoToSave,index=False,header=True)
+save_path = f"{paths.path_am_mass}{sim}_{phys}_{snapshot}.csv"
