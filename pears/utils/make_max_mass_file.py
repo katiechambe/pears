cac84083a08e79e28d9ee99e48432dd8b2793fff
katiechambe <katiechambe@email.arizona.edu>
2021-10-12 18:13:21 -0700

    reorganized, removed unaccessed imports, and changed to save hdf5 files

--- pears/utils/make_max_mass_file.py
+++ pears/utils/make_max_mass_file.py
@@ -8,21 +8,17 @@ How to run:
 python make_max_mass_file.py snapshot# simulation
 
 where snapshot can range between 0 and 136 for "Illustris"
-and 
+and 0 to 99 for "TNG"
 
 """
 __author__ = "Katie Chamberlain"
 __status__ = "Beta - forever~"
 __date__   = "September 2021"
 
-import numpy as np
 import h5py
 import sys
-import os
 from utils.merger_trees import TraceMergerTree
 from utils.get_groups import GetGroups
-from astropy.table import QTable
-import astropy.units as u
 
 snap = int(sys.argv[1])
 sim = str(sys.argv[2])
@@ -74,36 +70,17 @@ for phys in ["hydro","dark"]:
             max_mass_snapshot.append(max_mass_info.maxmass[1])
             current_mass.append(max_mass_info.masses[0])
 
-    file_name = f"{groups.path_data}max_masses/{sim}_{phys}_{snap}.ecsv"
-    if os.path.exists(file_name):
-        os.remove(file_name)
+    save_path = f"{groups.path_data}max_masses/{sim}_{phys}_{snap}.hdf5"
+    
+    data_dict = {"Subhalo ID":subids, 
+             "Max Mass":max_mass, 
+             "Max Mass Snap":max_mass_snapshot, 
+             "Current Snap Mass":current_mass}
 
-    # saving file as hdf5
-    # savefile = h5py.File(file_name, "w")
-    # subs = savefile.create_dataset("Subhalo ID", 
-    #                                dtype=np.int64, 
-    #                                data=subids)
-    # maxs = savefile.create_dataset("Max Mass", 
-    #                                dtype=np.float64, 
-    #                                data=max_mass)
-    # snaps = savefile.create_dataset("Max Mass Snap", 
-    #                                 dtype=np.int64, 
-    #                                 data=max_mass_snapshot)
-    # masss = savefile.create_dataset("Current Snap Mass", 
-    #                                 dtype=np.float64, 
-    #                                 data=current_mass)
+    f = h5py.File(save_path, 'w')
+    for key, val in data_dict.items():
+        f[key] = val
+    f.close()
 
-    # for i in [maxs,masss]:
-    #     i.attrs['units'] = "1e10 Msun"
-    # savefile.close()
-
-    # saving file as ecsv
-    t = QTable()
-    t["Subhalo ID"] = subids
-    t["Max Mass"] = max_mass * u.Unit(1e10*u.Msun)
-    t["Max Mass Snap"] = max_mass_snapshot
-    t["Current Snap Mass"] = current_mass * u.Unit(1e10*u.Msun)
-    t.write(f"{groups.path_data}max_masses/{sim}_{phys}_{snap}.csv",
-            overwrite=True)
-
+    print(f"Saved at {groups.path_data}max_masses/{sim}_{phys}_{snap}.hdf5")
     
