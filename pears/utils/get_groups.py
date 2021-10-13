c2d2693c361d0a56abf0f9a98d69d3098f143b3d
katiechambe <katiechambe@email.arizona.edu>
2021-10-13 13:43:46 -0700

    changed file type to hdf5 and restructured save groups - NOTE: still need to work on subhalo part

--- pears/utils/get_groups.py
+++ pears/utils/get_groups.py
@@ -10,11 +10,9 @@ __author__ = "Katie Chamberlain"
 __status__ = "Beta - forever~"
 __date__   = "September 2021"
 
+import h5py
 import numpy as np
 from utils.read_group_cats import ReadCats
-from utils.merger_trees import TraceMergerTree
-from astropy.table import QTable
-import astropy.units as u
 
 class GetGroups:
 
@@ -108,8 +106,49 @@ class GetGroups:
         self.subhalo_ids = np.where(subhalo_group_mask&subhalo_mass_mask)[0]
         self.subhalo_masses = self.submass_phys[self.subhalo_ids]
 
-        self.save_path = f"{self.sim}_{self.physics}_{self.size}_{self.snapshot}.ecsv"
-    
+        self.save_path = f"{self.sim}_{self.physics}_{self.snapshot}.hdf5"
+
+        self.header_dict = {"Snapshot":self.snapshot,
+             "Redshift":self.redshift,
+             "Simulation": self.sim,
+             "Physics": self.physics,
+             "Mass scale": self.size}
+
+
+    def save_groups(self):
+        '''
+        Save the groups that pass the mass cuts
+        '''
+        group_dict = {"Group Number":self.pass_numbers, 
+             "Group Mass":self.pass_mvir, 
+             "Group Radius":self.pass_rvir, 
+             "Nsubs":self.pass_nsubs}
+
+        units_dict = {"Group Number":"Group Number in Subfind Catalogs", 
+             "Group Mass":"Physical mass from Group_M_TopHat200 -- 1e10 Msun", 
+             "Group Radius":"Physical radius from Group_R_TopHat200 -- kpc", 
+             "Nsubs":"Number of subhalos in group"}
+
+        f = h5py.File(f"{self.path_groups}{self.save_path}", 'w')
+
+        header = f.create_dataset('Header')
+        for key, val in self.header_dict.items():
+            header[key] = val
+
+        if self.size == "dwarf":
+            dataset_name = "Dwarf Groups"
+
+        elif self.size == "massive":
+            dataset_name = "Massive Groups"
+
+        groups = f.create_dataset(dataset_name)
+        for key, val in group_dict.items():
+            groups[key] = val
+            groups.attrs[key] = units_dict[key]
+
+        f.close()
+        
+        print(f"Saved groups at {self.path_groups}{self.save_path}")
 
 
 # TODO: fix this up! 
@@ -200,30 +239,4 @@ class GetGroups:
         print(f"Saved subhalos at {self.sim}_{self.physics}_{self.size}_{self.snapshot}.ecsv")
 
 
-    def save_groups(self):
-        '''
-        Save the groups that pass the mass cuts
-        '''
-        group_number = u.Quantity(self.pass_numbers,
-                                  dtype=np.int64)
-        group_number.info.description = "Group Number"
 
-        group_mass = self.pass_mvir * u.Unit(1e10 * u.Msun)
-        group_mass.info.description = "Physical mass from Group_M_TopHat200"
-
-        group_radius = self.pass_rvir * u.kpc
-        group_radius.info.description = "Physical radius from Group_R_TopHat200"
-
-        group_nsubs = u.Quantity(self.pass_nsubs,
-                                 dtype=np.int64)
-        group_nsubs.info.description = "Number of subhalos in group"
-
-        t = QTable(meta={"snapshot":self.snapshot, "redshift":self.redshift})
-        t['group_number'] = group_number
-        t['group_mass'] = group_mass
-        t['group_radius'] = group_radius
-        t['group_nsubs'] = group_nsubs
-        t.write(self.path_groups + self.save_path,
-                overwrite=True)
-
-        print(f"Saved groups at data/groups/{self.sim}_{self.physics}_{self.size}_{self.snapshot}.ecsv")
