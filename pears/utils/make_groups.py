62aa233aed7d20bdfe1ac9d8f0bec4979d83a063
katie <katiechambe@email.arizona.com>
2021-10-25 14:34:57 -0700

    accidentally forced Illustris instead of sim

--- pears/utils/make_groups.py
+++ pears/utils/make_groups.py
@@ -3,8 +3,11 @@ from utils.get_groups import GetGroups
 
 snaps = {}
 
-snaps["Illustris"] = np.arange(0,136,1)
-snaps["TNG"] = np.arange(0,100)
+#snaps["Illustris"] = np.arange(0,136,1)
+#snaps["TNG"] = np.arange(0,100)
+
+snaps["Illustris"] = np.arange(130,136,1)
+snaps["TNG"] = np.arange(95,100,1)
 
 for sim in ["Illustris","TNG"]:
     for i in snaps[sim]:
@@ -12,7 +15,7 @@ for sim in ["Illustris","TNG"]:
             for s in ["dwarf","massive"]:
                 try:
                     GetGroups( snapshot=i, 
-                            sim="Illustris",
+                            sim=sim,
                             physics=x,
                             size=s).save_groups()
                 except AttributeError:
