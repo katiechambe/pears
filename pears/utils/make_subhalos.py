979cbe139bd7c73c8e1c4c6b06db335b5fb1d112
katiechambe <katiechambe@email.arizona.edu>
2022-06-07 16:27:31 -0700

    get subhalos because I couldnt add stellar mass to the subhalo data easily

--- pears/utils/make_subhalos.py
+++ pears/utils/make_subhalos.py
@@ -10,108 +10,110 @@ from utils.paths import SetupPaths
 
 paths = SetupPaths()
 
-snaps = {}
+# snaps = {}
 
-snaps["Illustris"] = np.arange(0,136,1)
-snaps["TNG"] = np.arange(0,100)
+snap = int(sys.argv[1])
+sim = str(sys.argv[2])
 
-for sim in ["Illustris","TNG"][1:]:
-    for snap in snaps[sim]:
-        #create the hdf5 file!
-        savepath = f"{sim}_{snap}.hdf5"
-        f = h5py.File(f"{paths.path_subhalos}{savepath}", 'w')
+#create the hdf5 file!
+savepath = f"{sim}_{snap}.hdf5"
+f = h5py.File(f"{paths.path_subhalos}{savepath}", 'w')
 
-        success=False
-        for phys in ["dark","hydro"]:
+success=False
+for phys in ["dark","hydro"]:
+    try:
+        am_file_path = f"{sim}_{phys}_{snap}.hdf5"
+        am_masses = h5py.File(f"{paths.path_am_mass}{am_file_path}", "r")
+        am_dict = {}
+        for key,val in am_masses.items():
+            am_dict[key] = np.array(val)
+
+        for s in ["dwarf","massive"]:
             try:
-                am_file_path = f"{sim}_{phys}_{snap}.hdf5"
-                am_masses = h5py.File(f"{paths.path_am_mass}{am_file_path}", "r")
-                am_dict = {}
-                for key,val in am_masses.items():
-                    am_dict[key] = np.array(val)
+                inst = GetGroups( snapshot=snap, 
+                        sim=sim,
+                        physics=phys,
+                        size=s)
 
-                for s in ["dwarf","massive"]:
-                    try:
-                        inst = GetGroups( snapshot=snap, 
-                                sim=sim,
-                                physics=phys,
-                                size=s)
+                maxmasses, maxmasssnaps = ([] for i in range(2))
+                medstellars, stellars =[],[]
+                gid, gmvir, grvir, gnsubs  = ([] for i in range(4))
 
-                        maxmasses, maxmasssnaps, stellars = ([] for i in range(3))
-                        gid, gmvir, grvir, gnsubs  = ([] for i in range(4))
+                for i in inst.subhalo_ids:
+                    # get the max mass and stellar mass data
+                    ind = np.where( am_dict['Subhalo ID'] == i )[0][0]
+                    maxmasses.append( am_dict['Max Mass'][ind] )
+                    maxmasssnaps.append( am_dict['Max Mass Snap'][ind])
+                    stellars.append( am_dict['Stellar Masses'][ind] )
+                    medstellars.append( am_dict['Median Stellar Mass'][ind] )
 
-                        for i in inst.subhalo_ids:
-                            # get the max mass and stellar mass data
-                            ind = np.where( am_dict['Subhalo ID'] == i )[0][0]
-                            maxmasses.append( am_dict['Max Mass'][ind] )
-                            maxmasssnaps.append( am_dict['Max Mass Snap'][ind])
-                            stellars.append( am_dict['Stellar Masses'][ind] )
+                    # get the data about the group each subhalo belongs to
+                    groupnum = am_dict['Group ID'][ind]
+                    gid.append(groupnum)
+                    gmvir.append(inst.mvirs_phys[groupnum])
+                    grvir.append(inst.rvirs_phys[groupnum])
+                    gnsubs.append(inst.nsubs[groupnum])
 
-                            # get the data about the group each subhalo belongs to
-                            groupnum = am_dict['Group ID'][ind]
-                            gid.append(groupnum)
-                            gmvir.append(inst.mvirs_phys[groupnum])
-                            grvir.append(inst.rvirs_phys[groupnum])
-                            gnsubs.append(inst.nsubs[groupnum])
-
-                        sub_dict = {"Group ID":np.array(gid), 
-                                    "Group Mass":np.array(gmvir), 
-                                    "Group Radius":np.array(grvir), 
-                                    "Nsubs":np.array(gnsubs),
-                                    "Subhalo ID":inst.subhalo_ids,
-                                    "Subhalo Mass":inst.subhalo_masses,
-                                    "Subhalo Pos":inst.subhalo_pos,
-                                    "Subhalo Vel":inst.subhalo_vel,
-                                    "Subhalo Max Mass":np.array(maxmasses),
-                                    "Subhalo Max Mass Snap":np.array(maxmasssnaps),
-                                    "Subhalo Stellar Masses":np.array(stellars)
-                                    }
-
-                        units_dict = {
-                            "Group ID":"Group Number in Subfind Catalogs", 
-                            "Group Mass":"Physical mass from Group_M_TopHat200 -- 1e10 Msun", 
-                            "Group Radius":"Physical radius from Group_R_TopHat200 -- kpc", 
-                            "Nsubs":"Number of subhalos in group",
-                            "Subhalo ID": "Subhalo ID at current snapshot",
-                            "Subhalo Mass": "Subhalo mass at current snapshot -- 1e10 Msun",
-                            "Subhalo Pos":"Subhalo physical position in box x,y,z -- kpc",
-                            "Subhalo Vel":"Subhalo velocity in vx, vy, vz -- km/s",
-                            "Subhalo Max Mass": "Maximum mass ever achieved by the subhalo -- 1e10 Msun",
-                            "Subhalo Max Mass Snap": "Snapshot at which maximum halo mass is achieved",
-                            "Subhalo Stellar Masses": "Stellar masses from abundance matching -- 1e10 Msun"
+                sub_dict = {"Group ID":np.array(gid), 
+                            "Group Mass":np.array(gmvir), 
+                            "Group Radius":np.array(grvir), 
+                            "Nsubs":np.array(gnsubs),
+                            "Subhalo ID":inst.subhalo_ids,
+                            "Subhalo Mass":inst.subhalo_masses,
+                            "Subhalo Pos":inst.subhalo_pos,
+                            "Subhalo Vel":inst.subhalo_vel,
+                            "Subhalo Max Mass":np.array(maxmasses),
+                            "Subhalo Max Mass Snap":np.array(maxmasssnaps),
+                            "Subhalo Stellar Masses":np.array(stellars),
+                            "Subhalo Med Stellar Mass":np.array(medstellars)
                             }
 
-                        for key, val in sub_dict.items():
-                            dset = f.create_dataset(f'/{phys}/{s}/{key}', 
-                                                    shape=val.shape,
-                                                    dtype=val.dtype)
-                            dset.attrs[key] = units_dict[key]
-                            dset[:] = val
+                units_dict = {
+                    "Group ID":"Group Number in Subfind Catalogs", 
+                    "Group Mass":"Physical mass from Group_M_TopHat200 -- 1e10 Msun", 
+                    "Group Radius":"Physical radius from Group_R_TopHat200 -- kpc", 
+                    "Nsubs":"Number of subhalos in group",
+                    "Subhalo ID": "Subhalo ID at current snapshot",
+                    "Subhalo Mass": "Subhalo mass at current snapshot -- 1e10 Msun",
+                    "Subhalo Pos":"Subhalo physical position in box x,y,z -- kpc",
+                    "Subhalo Vel":"Subhalo velocity in vx, vy, vz -- km/s",
+                    "Subhalo Max Mass": "Maximum mass ever achieved by the subhalo -- 1e10 Msun",
+                    "Subhalo Max Mass Snap": "Snapshot at which maximum halo mass is achieved",
+                    "Subhalo Stellar Masses": "Stellar masses from abundance matching -- 1e10 Msun",
+                    "Subhalo Med Stellar Mass":"Median stellar mass from abundance matching -- 1e10 Msun"
+                    }
 
-                        success = True
+                for key, val in sub_dict.items():
+                    dset = f.create_dataset(f'/{phys}/{s}/{key}', 
+                                            shape=val.shape,
+                                            dtype=val.dtype)
+                    dset.attrs[key] = units_dict[key]
+                    dset[:] = val
 
-                    except AttributeError:
-                        print(f"Cannot save {sim} {phys} {s} for snapshot {snap}")
-                    except OSError:
-                        print(f"Cannot save {sim} {phys} {s} for snapshot {snap} - DNE")
+                success = True
+
+            except AttributeError:
+                print(f"Cannot save {sim} {phys} {s} for snapshot {snap}")
             except OSError:
-                print(f"Cannot find AM file for {sim} {phys} for snapshot {snap}")
-                print("Closed file")
+                print(f"Cannot save {sim} {phys} {s} for snapshot {snap} - DNE")
+    except OSError:
+        print(f"Cannot find AM file for {sim} {phys} for snapshot {snap}")
+        print("Closed file")
 
-        if success:
-            #create header with simulation info
-            header_dict = {"Snapshot":inst.snapshot,
-                "Redshift":inst.redshift,
-                "Simulation":inst.sim}
+if success:
+    #create header with simulation info
+    header_dict = {"Snapshot":inst.snapshot,
+        "Redshift":inst.redshift,
+        "Simulation":inst.sim}
 
-            dset = f.create_group('/Header')
-            for key in header_dict.keys():
-                dset.attrs[key] = header_dict[key]
+    dset = f.create_group('/Header')
+    for key in header_dict.keys():
+        dset.attrs[key] = header_dict[key]
 
-            f.close()
-            print(f"Saved groups at {paths.path_groups}{sim}_{snap}.hdf5")
-        
-        else:
-            print("Closing file")
-            f.close()
-            print(f"Something went wrong at {paths.path_groups}{sim}_{snap}.hdf5")
+    f.close()
+    print(f"Saved groups at {paths.path_groups}{sim}_{snap}.hdf5")
+
+else:
+    print("Closing file")
+    f.close()
+    print(f"Something went wrong at {paths.path_groups}{sim}_{snap}.hdf5")
